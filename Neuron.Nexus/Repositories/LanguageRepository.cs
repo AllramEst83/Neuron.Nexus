1e84a6879bea949b4f112e9761b3e2995a5e25be
Kay Wiberg <kaywib@gmail.com>
2023-07-29 13:11:16 +0200

    Added more supported countries

--- Neuron.Nexus/Repositories/LanguageRepository.cs
+++ Neuron.Nexus/Repositories/LanguageRepository.cs
@@ -1,10 +1,4 @@
 ﻿using Neuron.Nexus.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Neuron.Nexus.Repositories
 {
@@ -17,6 +11,12 @@ namespace Neuron.Nexus.Repositories
         public LanguageRepository()
         {
         }
+
+        /// <summary>
+        /// Compatible langugages
+        /// https://learn.microsoft.com/en-us/azure/ai-services/speech-service/language-support?tabs=stt
+        /// </summary>
+        /// <returns></returns>
         public List<LanguageOption> GetLanguages()
         {
             return new List<LanguageOption>
@@ -24,8 +24,15 @@ namespace Neuron.Nexus.Repositories
                 new LanguageOption() {Id = 1,FullLanguageCode = "sv-SE", ShortLanguageCode = "sv" ,LanguageName = "Swedish", NativeLanguageName = "Svenska" },
                 new LanguageOption() {Id = 2,FullLanguageCode = "en-US", ShortLanguageCode = "en", LanguageName = "English", NativeLanguageName = "English"},
                 new LanguageOption() {Id = 3,FullLanguageCode = "tr-TR", ShortLanguageCode = "tr", LanguageName = "Turkish ", NativeLanguageName = "Türkçesi "},
-                new LanguageOption() {Id = 3,FullLanguageCode = "es-ES", ShortLanguageCode = "es", LanguageName = "Spanish ", NativeLanguageName = "Español "},
-                new LanguageOption() {Id = 3,FullLanguageCode = "fr-FR", ShortLanguageCode = "fr", LanguageName = "French ", NativeLanguageName = "Français"}
+                new LanguageOption() {Id = 4,FullLanguageCode = "es-ES", ShortLanguageCode = "es", LanguageName = "Spanish ", NativeLanguageName = "Español "},
+                new LanguageOption() {Id = 5,FullLanguageCode = "fr-FR", ShortLanguageCode = "fr", LanguageName = "French ", NativeLanguageName = "Français"},
+                new LanguageOption() {Id = 6,FullLanguageCode = "bs-BA", ShortLanguageCode = "bs", LanguageName = "Bosnian", NativeLanguageName = "Bosnia"},
+                new LanguageOption() {Id = 7,FullLanguageCode = "de-DE", ShortLanguageCode = "de", LanguageName = "German ", NativeLanguageName = "Deutsch"},
+                new LanguageOption() {Id = 8,FullLanguageCode = "ru-RU", ShortLanguageCode = "ru", LanguageName = "Russian", NativeLanguageName = "Russkie "},
+                new LanguageOption() {Id = 9,FullLanguageCode = "uk-UA", ShortLanguageCode = "uk", LanguageName = "Ukrainian", NativeLanguageName = "Ukrajina"},
+                new LanguageOption() {Id = 10,FullLanguageCode = "fi-FI", ShortLanguageCode = "fi", LanguageName = "Finnish", NativeLanguageName = "Suomi"},
+                new LanguageOption() {Id = 10,FullLanguageCode = "ar-IQ", ShortLanguageCode = "ar", LanguageName = "Arabic (Iraq)", NativeLanguageName = "al-ʿIrāq"}
+
             };
         }
     }
