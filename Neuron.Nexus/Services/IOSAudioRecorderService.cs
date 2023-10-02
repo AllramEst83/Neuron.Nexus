33bb929e0af3f86c0339a60b71aa35e697b07658
Kay Wiberg <kaywib@gmail.com>
2023-10-02 14:45:55 +0200

    Refactored some code in IOSAudioRecorderService with copilot

--- Neuron.Nexus/Services/IOSAudioRecorderService.cs
+++ Neuron.Nexus/Services/IOSAudioRecorderService.cs
@@ -1,6 +1,7 @@
 ﻿#if IOS
 using AVFoundation;
 using Foundation;
+using System.IO;
 
 namespace Neuron.Nexus.Services
 {
@@ -14,81 +15,72 @@ namespace Neuron.Nexus.Services
 
     public class IOSAudioRecorderService : IAudioRecorderService
     {
-     //https://www.syncfusion.com/blogs/post/building-an-audio-recorder-and-player-app-in-net-maui.aspx
+        //https://www.syncfusion.com/blogs/post/building-an-audio-recorder-and-player-app-in-net-maui.aspx
 
-        AVAudioRecorder recorder;
-        NSUrl url;
-        NSError error;
-        NSDictionary settings;
-        string audioFilePath;
+        readonly AVAudioRecorder recorder;
+        readonly NSUrl url;
+        readonly NSError error;
+        readonly NSDictionary settings;
+        readonly string audioFilePath;
 
         public IOSAudioRecorderService()
-        {
-            InitializeAudioSession();
-        }
-        private bool InitializeAudioSession()
         {
             var audioSession = AVAudioSession.SharedInstance();
             var err = audioSession.SetCategory(AVAudioSessionCategory.PlayAndRecord);
             if (err != null)
             {
                 Console.WriteLine("audioSession: {0}", err);
-                return false;
+                return;
             }
             err = audioSession.SetActive(true);
             if (err != null)
             {
                 Console.WriteLine("audioSession: {0}", err);
-                return false;
+                return;
             }
-            return false;
+
+            string fileName = "/Record_" + DateTime.UtcNow.ToString("ddMMM_hhmmss") + ".wav";
+            var docuFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            audioFilePath = docuFolder + fileName;
+            url = NSUrl.FromFilename(audioFilePath);
+
+            NSObject[] values = new NSObject[]
+            {
+                      NSNumber.FromFloat(44100.0f),
+                      NSNumber.FromInt32((int)AudioToolbox.AudioFormatType.LinearPCM),
+                      NSNumber.FromInt32(1),
+                      NSNumber.FromInt32(16),
+                      NSNumber.FromBoolean(false),
+                      NSNumber.FromBoolean(false)
+            };
+            NSObject[] key = new NSObject[]
+            {
+                      AVAudioSettings.AVSampleRateKey,
+                      AVAudioSettings.AVFormatIDKey,
+                      AVAudioSettings.AVNumberOfChannelsKey,
+                      AVAudioSettings.AVLinearPCMBitDepthKey,
+                      AVAudioSettings.AVLinearPCMIsBigEndianKey,
+                      AVAudioSettings.AVLinearPCMIsFloatKey
+            };
+            settings = NSDictionary.FromObjectsAndKeys(values, key);
+
+            recorder = AVAudioRecorder.Create(url, new AudioSettings(settings), out error);
+            recorder.PrepareToRecord();
         }
 
-        public void PauseRecord()
-        {
-            recorder.Pause();
-        }
+        public void PauseRecord() => recorder.Pause();
+
         public void ResetRecord()
         {
-            recorder.Dispose();
-            recorder = null;
-        }
-        public void StartRecord()
-        {
-            if (recorder == null)
+            if (recorder != null)
             {
-                string fileName = "/Record_" + DateTime.UtcNow.ToString("ddMMM_hhmmss") + ".wav";
-                var docuFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                audioFilePath = docuFolder + fileName;
-                url = NSUrl.FromFilename(audioFilePath);
-                NSObject[] values = new NSObject[]
-                {
-              NSNumber.FromFloat(44100.0f),
-              NSNumber.FromInt32((int)AudioToolbox.AudioFormatType.LinearPCM),
-              NSNumber.FromInt32(1),
-              NSNumber.FromInt32(16),
-              NSNumber.FromBoolean(false),
-              NSNumber.FromBoolean(false)
-                };
-                NSObject[] key = new NSObject[]
-                {
-              AVAudioSettings.AVSampleRateKey,
-              AVAudioSettings.AVFormatIDKey,
-              AVAudioSettings.AVNumberOfChannelsKey,
-              AVAudioSettings.AVLinearPCMBitDepthKey,
-              AVAudioSettings.AVLinearPCMIsBigEndianKey,
-              AVAudioSettings.AVLinearPCMIsFloatKey
-                };
-                settings = NSDictionary.FromObjectsAndKeys(values, key);
-                recorder = AVAudioRecorder.Create(url, new AudioSettings(settings), out error);
-                recorder.PrepareToRecord();
-                recorder.Record();
-            }
-            else
-            {
-                recorder.Record();
+                recorder.Dispose();
+                File.Delete(audioFilePath);
             }
         }
+
+        public void StartRecord() => recorder.Record();
+
         public string StopRecord()
         {
             if (recorder == null)
@@ -96,7 +88,6 @@ namespace Neuron.Nexus.Services
                 return string.Empty;
             }
             recorder.Stop();
-            recorder = null;
             return audioFilePath;
         }
     }
