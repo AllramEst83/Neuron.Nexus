dc127b47285ff4d6d593438c1c0b644fc0e7d72c
Kay Wiberg <kaywib@gmail.com>
2024-02-23 13:02:01 +0100

    Added forcing of portrait

--- Neuron.Nexus/Platforms/Android/MainActivity.cs
+++ Neuron.Nexus/Platforms/Android/MainActivity.cs
@@ -1,10 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 
 namespace Neuron.Nexus;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;
+    }
+
 }
 
